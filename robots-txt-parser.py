8914535f53c2d0bcd3585c0900152bbc02f6ca63
Kamil Gorny <kamilgorny@MacBook-Air-Kamil.local>
2023-05-07 22:40:29 +0200

    feat: Add host and useragent arguments

--- robots-txt-parser.py
+++ robots-txt-parser.py
@@ -1,4 +1,11 @@
 import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-u", "--useragent", help="User Agent")
+parser.add_argument("-host", "--host", help="Webiste Url")
+
 
 def parse():
     url = "http://www.tripadvisor.com/robots.txt"
