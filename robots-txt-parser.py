b4c0a6a28d9f5a282c8f00da6e5f28c826c6774f
Kamil Gorny <kamilgorny@MacBook-Air-Kamil.local>
2023-05-08 06:58:37 +0200

    feat: Parse robots.txt content

--- robots-txt-parser.py
+++ robots-txt-parser.py
@@ -3,17 +3,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-u", "--useragent", help="User Agent")
-parser.add_argument("-host", "--host", help="Webiste Url")
+parser.add_argument("-u", "--useragent", dest= "useragent", help="User Agent")
+parser.add_argument("-host", "--host", dest="host", help="Webiste Url")
 
+args = parser.parse_args()
 
 def parse():
-    url = "http://www.tripadvisor.com/robots.txt"
+    url = args.host + "/robots.txt"
     headers = {
-    'User-Agent': 'bugcrowd',
+    'User-Agent': args.useragent,
     }
     res = requests.get(url, headers=headers)
     
-    print(res.text)
+    response_content = res.text
+    response_parsed = ""
+
+    for line in response_content.splitlines():
+        if not line.startswith("User-Agent:") and not len(line.strip()) == 0:
+            response_parsed += line.replace("Disallow: ", args.host).replace("Allow: ", args.host) + "\n"
+
+    print(response_parsed)
 
 parse()
